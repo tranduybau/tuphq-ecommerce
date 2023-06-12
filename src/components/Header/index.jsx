a8164779b0d88fb5988b2dc6feca6acb76670f44
Tu <tuphan694@gmail.com>
2023-06-12 12:04:09 +0700

    fix header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,46 +3,90 @@ import { useState } from 'react'
 import './Header.scss'
 import Image from 'next/image'
 import classNames from 'classnames'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
     const [active, setActive] = useState(1)
+    console.log(screen.width)
     return (
         <header className='header'>
             <div className='top-header'>
-                <div className="top-header-main ">
-                    <div className='col-xl-2 col-lg-2 col-md-1'></div>
-                    <div className='top-header__text col-xl-8 col-lg-7 col-md-9'>
-                        <span className='top-header__text-content'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</span>
-                        <a href='#' className='top-header__text-link'>ShopNow</a>
+                <div className="top-header-main container">
+                    <div className='top-header__text '>
+                        <span className='top-header__text-content font-poppins'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</span>
+                        <a href='#' className='top-header__text-link font-poppins'>ShopNow</a>
                     </div>
-                    <div className='top-header__language col-xl-2 col-lg-3 col-md-2'>
+                    <div className='top-header__language '>
                         <select className="language-switcher-select">
-                            <option value="en">English</option>
-                            <option value="vn">Vietnamese</option>
+                            <option className='font-poppins' value="en">English</option>
+                            <option className='font-poppins' value="vn">Vietnamese</option>
                         </select>
                     </div>
                 </div>
             </div>
-            <div className='main-header container'>
-                <div className='main-header__wrapper row'>
-                    <div className='main-header__brand col-xl-3 col-lg-2 col-md-5 col-sm-3 col-xs-2'>
-                        <span>Exclusive</span>
+            <div className='bars-view'>
+                        <ul className="bars-view__menu">
+                            <li className="nav-item">
+                                <a onClick={() => setActive(1)} className={classNames('nav-link', 'main-header-link', 'font-poppins', { active: active === 1 })} href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a onClick={() => setActive(2)} className={classNames('nav-link', 'main-header-link', 'font-poppins', { active: active === 2 })} href="#">Contact</a>
+                            </li>
+                            <li className="nav-item">
+                                <a onClick={() => setActive(3)} className={classNames('nav-link', 'main-header-link', 'font-poppins', { active: active === 3 })} href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a onClick={() => setActive(4)} className={classNames('nav-link', 'main-header-link', 'font-poppins', { active: active === 4 })} href="#">Sign up</a>
+                            </li>
+                        </ul>
+                        <div className='bars-view__actions'>
+                            <div className='main-header__actions-search'>
+                                <input placeholder='What are you looking for?' />
+                                <Image
+                                    className='icon-search'
+                                    width={24}
+                                    height={24}
+                                    src='image/SearchIcon.svg'
+                                    alt="search"
+                                />
+                            </div>
+                            <Image
+                                className='icon-heart'
+                                width={32}
+                                height={32}
+                                alt='Heart'
+                                src='image/Heart.svg'
+                            />
+                            <Image
+                                className='icon-cart'
+                                width={32}
+                                height={32}
+                                alt='Cart'
+                                src='image/Cart.svg'
+                            />
+                        </div>
                     </div>
-                    <ul className="main-header__menu col-xl-4 col-lg-5 col-md-7 col-sm-9 col-xs-10">
+            <div className='main-header container'>
+                <div className='main-header__wrapper'>
+                    <div className='main-header__brand'>
+                        <span className='font-inter'>Exclusive</span>
+                    </div>
+                    <ul className="main-header__menu">
                         <li className="nav-item">
-                            <a onClick={() => setActive(1)} className={classNames('nav-link', 'main-header-link', { active: active === 1 })} href="#">Home</a>
+                            <a onClick={() => setActive(1)} className={classNames('nav-link', 'main-header-link', 'font-poppins', { active: active === 1 })} href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a onClick={() => setActive(2)} className={classNames('nav-link', 'main-header-link', { active: active === 2 })} href="#">Contact</a>
+                            <a onClick={() => setActive(2)} className={classNames('nav-link', 'main-header-link', 'font-poppins', { active: active === 2 })} href="#">Contact</a>
                         </li>
                         <li className="nav-item">
-                            <a onClick={() => setActive(3)} className={classNames('nav-link', 'main-header-link', { active: active === 3 })} href="#">About</a>
+                            <a onClick={() => setActive(3)} className={classNames('nav-link', 'main-header-link', 'font-poppins', { active: active === 3 })} href="#">About</a>
                         </li>
                         <li className="nav-item">
-                            <a onClick={() => setActive(4)} className={classNames('nav-link', 'main-header-link', { active: active === 4 })} href="#">Sign up</a>
+                            <a onClick={() => setActive(4)} className={classNames('nav-link', 'main-header-link', 'font-poppins', { active: active === 4 })} href="#">Sign up</a>
                         </li>
                     </ul>
-                    <div className='main-header__actions col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12'>
+                    <div className='main-header__actions'>
                         <div className='main-header__actions-search'>
                             <input placeholder='What are you looking for?' />
                             <Image
@@ -51,7 +95,7 @@ export default function Header() {
                                 height={24}
                                 src='image/SearchIcon.svg'
                                 alt="search"
-                            ></Image>
+                            />
                         </div>
                         <Image
                             className='icon-heart'
@@ -59,16 +103,22 @@ export default function Header() {
                             height={32}
                             alt='Heart'
                             src='image/Heart.svg'
-                        ></Image>
+                        />
                         <Image
                             className='icon-cart'
                             width={32}
                             height={32}
                             alt='Cart'
                             src='image/Cart.svg'
-                        ></Image>
+                        />
                     </div>
+                    <div className='bar-icon'>
+                        <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+                    </div>
+
+                    
                 </div>
+
             </div>
         </header>
     )
